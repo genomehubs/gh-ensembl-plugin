7e057128a44f55d98ed3f6e4266803a85ea04ab1
Richard Challis <rjchallis@gmail.com>
2017-06-20 13:31:15 +0100

    update ImageConfig

--- modules/EnsEMBL/Web/ImageConfig.pm
+++ modules/EnsEMBL/Web/ImageConfig.pm
@@ -1,6 +1,6 @@
 =head1 LICENSE
 
-Copyright [2009-2016] EMBL-European Bioinformatics Institute
+Copyright [2014-2017] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -43,7 +43,8 @@ sub menus {
 ##
 
 ## ParaSite
-    genomehubs_rnaseq    => 'RNA-Seq Alignments',
+    parasite_rnaseq     => 'RNA-Seq Alignments',
+    community_annotation => 'Community Annotation',
 ##
 
 ## EG used to organise fungi/protists external tracks
@@ -110,218 +111,10 @@ sub menus {
   };
 }
 
-sub _add_trackhub {
-  my ($self, $menu_name, $url, $is_poor_name, $existing_menu, $force_hide) = @_;
-
-  return ($menu_name, {}) if $self->{'_attached_trackhubs'}{$url};
-
-  my $trackhub  = EnsEMBL::Web::File::Utils::TrackHub->new('hub' => $self->hub, 'url' => $url);
-  my $hub_info = $trackhub->get_hub({'assembly_lookup' => $self->species_defs->assembly_lookup,
-                                      'parse_tracks' => 1}); ## Do we have data for this species?
-
-  if ($hub_info->{'error'}) {
-    ## Probably couldn't contact the hub
-    push @{$hub_info->{'error'}||[]}, '<br /><br />Please check the source URL in a web browser.';
-  } else {
-    my $shortLabel = $hub_info->{'details'}{'shortLabel'};
-    $menu_name = $shortLabel if $shortLabel and $is_poor_name;
-
-    my $menu     = $existing_menu || $self->tree->append_child($self->create_submenu($menu_name, $menu_name, { external => 1, trackhub_menu => 1 }));
-
-    my $node;
-    my $assemblies = $self->hub->species_defs->get_config($self->species,'TRACKHUB_ASSEMBLY_ALIASES');
-    $assemblies ||= [];
-    $assemblies = [ $assemblies ] unless ref($assemblies) eq 'ARRAY';
-    foreach my $assembly_var (qw(UCSC_GOLDEN_PATH ASSEMBLY_VERSION)) {
-      my $assembly = $self->hub->species_defs->get_config($self->species,$assembly_var);
-      next unless $assembly;
-      push @$assemblies,$assembly;
-    }
-    foreach my $assembly (@$assemblies) {
-      $node = $hub_info->{'genomes'}{$assembly}{'tree'};
-      last if $node;
-    }
-    if ($node) {
-      $self->_add_trackhub_node($node, $menu, $menu_name, $force_hide);
-
-      $self->{'_attached_trackhubs'}{$url} = 1;
-    } else {
-      my $assembly = $self->hub->species_defs->get_config($self->species, 'ASSEMBLY_VERSION');
-      $hub_info->{'error'} = ["No sources could be found for assembly $assembly. Please check the hub's genomes.txt file for supported assemblies."];
-    }
-  }
-  return ($menu_name, $hub_info);
-}
-
-sub _add_trackhub_tracks {
-  my ($self, $parent, $children, $config, $menu, $name) = @_;
-  my $hub    = $self->hub;
-  my $data   = $parent->data;
-  my $matrix = $config->{'dimensions'}{'x'} && $config->{'dimensions'}{'y'};
-  my %tracks;
-
-  my %options = (
-    menu_key     => $name,
-    menu_name    => $name,
-    submenu_key  => $self->tree->clean_id("${name}_$data->{'track'}", '\W'),
-    submenu_name => $data->{'shortLabel'},
-    trackhub      => 1,
-  );
-
-  if ($matrix) {
-    $options{'matrix_url'} = $hub->url('Config', { action => 'Matrix', function => $hub->action, partial => 1, menu => $options{'submenu_key'} });
-
-    foreach my $subgroup (keys %$config) {
-      next unless $subgroup =~ /subGroup\d/;
-
-      foreach (qw(x y)) {
-        if ($config->{$subgroup}{'name'} eq $config->{'dimensions'}{$_}) {
-          $options{'axis_labels'}{$_} = { %{$config->{$subgroup}} }; # Make a deep copy so that the regex below doesn't affect the subgroup config
-          s/_/ /g for values %{$options{'axis_labels'}{$_}};
-        }
-      }
-
-      last if scalar keys %{$options{'axis_labels'}} == 2;
-    }
-
-    $options{'axes'} = { map { $_ => $options{'axis_labels'}{$_}{'label'} } qw(x y) };
-  }
-
-  my $submenu = $self->create_submenu($options{'submenu_key'}, $options{'submenu_name'}, {
-    external => 1,
-    ($matrix ? (
-      menu   => 'matrix',
-      url    => $options{'matrix_url'},
-      matrix => {
-        section     => $menu->data->{'caption'},
-        header      => $options{'submenu_name'},
-        desc_url    => $config->{'description_url'},
-        description => $config->{'shortLabel'},
-        axes        => $options{'axes'},
-      }
-    ) : ())
-  });
-
-  $self->alphabetise_tracks($submenu, $menu);
-
-  my $count_visible = 0;
-
-  my $style_mappings = {
-                        'bigbed' => {
-                                      'full'    => 'as_transcript_label',
-                                      'pack'    => 'as_transcript_label',
-                                      'squish'  => 'half_height',
-                                      'dense'   => 'as_alignment_nolabel',
-                                      },
-                        'bigwig' => {
-                                      'full'    => 'signal',
-                                      'default' => 'signal',
-                                      'dense'   => 'compact',
-                                    },
-                        'vcf' =>    {
-                                      'full'    => 'histogram',
-                                      'dense'   => 'compact',
-                                    },
-                      };
-
-  foreach (@{$children||[]}) {
-    my $track        = $_->data;
-    my $type         = ref $track->{'type'} eq 'HASH' ? uc $track->{'type'}{'format'} : uc $track->{'type'};
-## ParaSite: show bigGenePred as bigBed
-    $type =~ s/^BIGGENEPRED$/BIGBED/;
-##
-
-    my $on_off = $config->{'on_off'} || $track->{'on_off'} || 'off';  ## ParaSite
-    ## Turn track on if there's no higher setting turning it off
-    if (!$config->{'on_off'} && !$track->{'on_off'}) {
-      $on_off = 'on';
-   }
-
-    my $ucsc_display  = $config->{'visibility'} || $track->{'visibility'};
-
-    ## FIXME - According to UCSC's documentation, 'squish' is more like half_height than compact
-    my $squish       = $track->{'visibility'} eq 'squish' || $config->{'visibility'} eq 'squish'; # FIXME: make it inherit correctly
-## ParaSite: change the way we display the labels
-    (my $source_name = $track->{'longLabel'}) =~ s/_/ /g;
-##
-
-    ## Translate between UCSC terms and Ensembl ones
-    my $default_display = $style_mappings->{lc($type)}{$ucsc_display}
-                              || $style_mappings->{lc($type)}{'default'}
-                              || 'off';  ## ParaSite: switch off the track by default if no display is given
-    $options{'default_display'} = $default_display;
-
-    ## Set track style if appropriate
-    if ($on_off && $on_off eq 'on') {
-      $options{'display'} = $default_display;
-      $count_visible++;
-    }
-    else {
-      $options{'display'} = 'off';
-    }
-
-    my $desc_url = $track->{'description_url'} ? $hub->url('Ajax', {'type' => 'fetch_html', 'url' => $track->{'description_url'}}) : '';
-
-    ## Note that we use a duplicate value in description and longLabel, because non-hub files
-    ## often have much longer descriptions so we need to distinguish the two
-    my $source       = {
-      name        => $track->{'track'},
-      source_name => $source_name,
-      desc_url    => $track->{'description_url'},
-      description => $desc_url ? qq(<span style="overflow-wrap: break-word" class="_dyna_load"><a class="hidden" href="$desc_url">$track->{'longLabel'}</a>Loading &#133;</span>) : '',
-      longLabel   => $track->{'longLabel'},
-      caption     => $track->{'shortLabel'},
-      source_url  => $track->{'bigDataUrl'},
-      colour      => exists $track->{'color'} ? $track->{'color'} : undef,
-      colorByStrand => exists $track->{'colorByStrand'} ? $track->{'colorByStrand'} : undef,
-      spectrum    => exists $track->{'spectrum'} ? $track->{'spectrum'} : undef,
-      no_titles   => $type eq 'BIGWIG', # To improve browser speed don't display a zmenu for bigwigs
-      squish      => $squish,
-      signal_range => $track->{'signal_range'},
-      %options
-    };
-
-    # Graph range - Track Hub default is 0-127
-
-    if (exists $track->{'viewLimits'}) {
-      $source->{'viewLimits'} = $track->{'viewLimits'};
-    } elsif ($track->{'autoScale'} eq 'off') {
-      $source->{'viewLimits'} = '0:127';
-    }
-
-    if (exists $track->{'maxHeightPixels'}) {
-      $source->{'maxHeightPixels'} = $track->{'maxHeightPixels'};
-    } elsif ($type eq 'BIGWIG' || $type eq 'BIGBED') {
-## ParaSite: change height of tracks
-      $source->{'maxHeightPixels'} = '60:60:60';
-## ParaSite
-    }
-
-    if ($matrix) {
-      my $caption = $track->{'shortLabel'};
-      $source->{'section'} = $parent->data->{'shortLabel'};
-      ($source->{'source_name'} = $track->{'longLabel'}) =~ s/_/ /g;
-      $source->{'labelcaption'} = $caption;
-
-      $source->{'matrix'} = {
-        menu   => $options{'submenu_key'},
-        column => $options{'axis_labels'}{'x'}{$track->{'subGroups'}{$config->{'dimensions'}{'x'}}},
-        row    => $options{'axis_labels'}{'y'}{$track->{'subGroups'}{$config->{'dimensions'}{'y'}}},
-      };
-
-      $source->{'column_data'} = { desc_url => $config->{'description_url'}, description => $config->{'shortLabel'}, no_subtrack_description => 1 };
-    }
-
-    $tracks{$type}{$source->{'name'}} = $source;
-  }
-
-  $self->load_file_format(lc, $tracks{$_}) for keys %tracks;
-}
-
 sub _update_missing {
   my ($self, $object) = @_;
   my $species_defs    = $self->species_defs;
-  my $count_missing   = grep { $_->get('display') eq 'off' || !$_->get('display') } $self->get_tracks;
+  my $count_missing   = grep { !$_->get('display') || $_->get('display') eq 'off' } @{$self->get_tracks};
   my $missing         = $self->get_node('missing');
 
   $missing->set('extra_height', 4) if $missing;
@@ -348,5 +141,56 @@ sub _update_missing {
   return { count => $count_missing, information => $info };
 }
 
+sub add_sequence_variations_eva {
+  my ($self, $key, $hashref) = @_;
+  return if $key ne 'core';
+
+  my $menu = $self->get_node('variation');
+  return unless $menu;
+
+  my $options = {
+    db         => $key,
+    glyphset   => '_eva',
+    strand     => 'r',
+    depth      => 0.5,
+    bump_width => 0,
+    colourset  => 'variation',
+    display    => 'off',
+    renderers  => [ 'off', 'Off', 'compact', 'Collapsed' ],
+  };
+
+  $self->add_sequence_variations_default_eva($key, $hashref, $options);
+
+  $self->add_track('information', 'variation_legend', 'Variant Legend', 'variation_legend', { strand => 'r' });
+
+}
+
+sub add_sequence_variations_default_eva {
+  my ($self, $key, $hashref, $options) = @_;
+  my $menu = $self->get_node('variation');
+  my $sequence_variation = ($menu->get_node('variants')) ? $menu->get_node('variants') : $self->create_submenu('variants', 'Sequence variants');
+
+  return unless $self->hub->species_defs->EVA_TRACKS;
+
+  foreach my $study (@{$self->hub->species_defs->EVA_TRACKS}) {
+    my $title = $study->{'name'};
+    my $name  = "variation_feature_eva_" . $study->{'study_id'};
+    $sequence_variation->append($self->create_track($name, $title, {
+      %$options,
+      caption     => $study->{'study_id'} . " Variations",
+      sources     => undef,
+      study_id    => $study->{'study_id'},
+      eva_species => $study->{'eva_species'},
+      description => sprintf('%s<br />Variants loaded from study <a href="%s/?eva-study=%s">%s</a> in the European Variation Archive.',
+                             $study->{'description'},
+                             $self->hub->species_defs->EVA_URL,
+                             $study->{'study_id'},
+                             $study->{'study_id'}),
+    }));
+  }
+
+  $menu->append($sequence_variation);
+
+}
 
 1;
--- modules/EnsEMBL/Web/ImageConfig/contigviewbottom.pm
+++ modules/EnsEMBL/Web/ImageConfig/contigviewbottom.pm
@@ -1,32 +1,40 @@
 =head1 LICENSE
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016] EMBL-European Bioinformatics Institute
+
+Copyright [2014-2017] EMBL-European Bioinformatics Institute
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
      http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
 =cut
 
 package EnsEMBL::Web::ImageConfig::contigviewbottom;
 
 use strict;
+use warnings;
 
-use base qw(EnsEMBL::Web::ImageConfig);
+use parent qw(EnsEMBL::Web::ImageConfig);
 
-sub init {
+sub init_cacheable {
   my $self = shift;
 
+  $self->SUPER::init_cacheable(@_);
+
   $self->set_parameters({
-    toolbars        => { top => 1, bottom => 1 },
-    sortable_tracks => 'drag', # allow the user to reorder tracks on the image
-    trackhubs        => 1,      # allow track hubs
-    opt_halfheight  => 0,      # glyphs are half-height [ probably removed when this becomes a track config ]
-    opt_lines       => 1,      # draw registry lines
+    image_resizeable => 1,
+    bottom_toolbar   => 1,
+    sortable_tracks  => 'drag', # allow the user to reorder tracks on the image
+    can_trackhubs    => 1,      # allow track hubs
+    opt_halfheight   => 0,      # glyphs are half-height [ probably removed when this becomes a track config ]
+    opt_lines        => 1,      # draw registry lines
   });
 
   # First add menus in the order you want them for this display
@@ -41,7 +49,9 @@ sub init {
     dna_align_est
     dna_align_rna
     dna_align_other
+    dna_align_ncrna_pred
     genomehubs_rnaseq
+    community_annotation
     protein_align
     protein_feature
     rnaseq
@@ -67,7 +77,6 @@ sub init {
     information
   ));
 
-  $self->image_resize = 1;
   my %desc = (
     contig    => 'Track showing underlying assembly contigs.',
     seq       => 'Track showing sequence in both directions. Only displayed at 1Kb and below.',
@@ -84,39 +93,6 @@ sub init {
   );
 
   $self->add_track('decorations', 'gc_plot', '%GC', 'gcplot', { display => 'normal',  strand => 'r', description => 'Shows percentage of Gs & Cs in region', sortable => 1 });
-
-  my $gencode_version = $self->hub->species_defs->GENCODE ? $self->hub->species_defs->GENCODE->{'version'} : '';
-  $self->add_track('transcript', 'gencode', "Basic Gene Annotations from GENCODE $gencode_version", '_gencode', {
-      labelcaption => "Genes (Basic set from GENCODE $gencode_version)",
-      display     => 'off',
-      description => 'The GENCODE set is the gene set for human and mouse. GENCODE Basic is a subset of representative transcripts (splice variants).',
-      sortable    => 1,
-      colours     => $self->species_defs->colour('gene'),
-      label_key  => '[biotype]',
-      logic_names => ['proj_ensembl',  'proj_ncrna', 'proj_havana_ig_gene', 'havana_ig_gene', 'ensembl_havana_ig_gene', 'proj_ensembl_havana_lincrna', 'proj_havana', 'ensembl', 'mt_genbank_import', 'ensembl_havana_lincrna', 'proj_ensembl_havana_ig_gene', 'ncrna', 'assembly_patch_ensembl', 'ensembl_havana_gene', 'ensembl_lincrna', 'proj_ensembl_havana_gene', 'havana'],
-      renderers   =>  [
-        'off',                     'Off',
-        'gene_nolabel',            'No exon structure without labels',
-        'gene_label',              'No exon structure with labels',
-        'transcript_nolabel',      'Expanded without labels',
-        'transcript_label',        'Expanded with labels',
-        'collapsed_nolabel',       'Collapsed without labels',
-        'collapsed_label',         'Collapsed with labels',
-        'transcript_label_coding', 'Coding transcripts only (in coding genes)',
-      ],
-    }) if($gencode_version);
-
-  if ($self->species_defs->ALTERNATIVE_ASSEMBLIES) {
-    foreach my $alt_assembly (@{$self->species_defs->ALTERNATIVE_ASSEMBLIES}) {
-      $self->add_track('misc_feature', "${alt_assembly}_assembly", "$alt_assembly assembly", 'alternative_assembly', {
-        display       => 'off',
-        strand        => 'f',
-        colourset     => 'alternative_assembly',
-        description   => "Track indicating $alt_assembly assembly",
-        assembly_name => $alt_assembly
-      });
-    }
-  }
 
   # Add in additional tracks
   $self->load_tracks;
@@ -139,16 +115,14 @@ sub init {
     $self->modify_configs($tracks, {display => 'compact'});
   }
 
-  # These tracks get added after the "auto-loaded tracks get addded
-  if ($self->species_defs->ENSEMBL_MOD) {
-    $self->add_track('information', 'mod', '', 'text', {
-      name    => 'Message of the day',
-      display => 'normal',
-      menu    => 'no',
-      strand  => 'r',
-      text    => $self->species_defs->ENSEMBL_MOD
-    });
+  ## ParaSite: display the EVA tracks by default
+  if($self->hub->species_defs->EVA_TRACKS) {
+    foreach my $study (@{$self->hub->species_defs->EVA_TRACKS}) {
+      my $track  = "variation_feature_eva_" . $study->{'study_id'};
+      $self->modify_configs([$track], {display => 'compact'});
+    }
   }
+  ##
 
   $self->add_tracks('information',
     [ 'missing', '', 'text', { display => 'normal', strand => 'r', name => 'Disabled track summary', description => 'Show counts of number of tracks turned off by the user' }],
@@ -160,24 +134,6 @@ sub init {
     [ 'ruler',     '', 'ruler',     { display => 'normal', strand => 'b', name => 'Ruler',     description => 'Shows the length of the region being displayed' }],
     [ 'draggable', '', 'draggable', { display => 'normal', strand => 'b', menu => 'no' }]
   );
-
-  ## LRG track
-  if ($self->species_defs->HAS_LRG) {
-    $self->add_tracks('lrg',
-      [ 'lrg_transcript', 'LRG', '_transcript', {
-        display     => 'off', # Switched off by default
-        strand      => 'b',
-        name        => 'LRG',
-        description => 'Transcripts from the <a class="external" href="http://www.lrg-sequence.org">Locus Reference Genomic sequence</a> project.',
-        logic_names => [ 'LRG_import' ],
-        logic_name  => 'LRG_import',
-        colours     => $self->species_defs->colour('gene'),
-        label_key   => '[display_label]',
-        colour_key  => '[logic_name]',
-        zmenu       => 'LRG',
-      }]
-    );
-  }
 
   ## Switch on multiple alignments defined in MULTI.ini
   my $compara_db      = $self->hub->database('compara');
@@ -195,22 +151,10 @@ sub init {
     }
   }
 
-  my @feature_sets = ('cisRED', 'VISTA', 'miRanda', 'NestedMICA', 'REDfly CRM', 'REDfly TFBS');
-
-  foreach my $f_set (@feature_sets) {
-    $self->modify_configs(
-      [ "regulatory_regions_funcgen_$f_set" ],
-      { depth => 25, height => 6 }
-    );
-  }
-
-  ## Regulatory build track now needs to be turned on explicitly
-  $self->modify_configs(['regbuild'], {display => 'compact'});
-
-  ## ParaSite
-    $self->modify_configs( ['dna_align_ncrna_pred'], {'display'=>'as_alignment_label'} );
-    $self->modify_configs( ['genomehubs_rnaseq'], {'strand'=>'r'} );
-  ##
+## ParaSite
+  $self->modify_configs( ['dna_align_ncrna_pred'], {'display'=>'as_alignment_label'} );
+  $self->modify_configs( ['genomehubs_rnaseq'], {'strand'=>'r'} );
+##
 
 }
 
